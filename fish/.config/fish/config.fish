f50451e3fa6e14849659c320ef1b68c09f827911
XXiaoA <isxxiaoa@gmail.com>
2022-07-20 11:33:30 +0800

    fish: use  zoxide instead of z.lua

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -5,9 +5,8 @@ export XDG_CONFIG_HOME=$HOME/.config
 export EDITOR="nvim"
 export VISUAL="nvim"
 
-starship init fish | source
 
 fish_vi_key_bindings
 
-# https://github.com/skywind3000/z.lua
-source (lua $XDG_CONFIG_HOME/fish/z.lua --init fish | psub)
+starship init fish | source
+zoxide init fish | source
