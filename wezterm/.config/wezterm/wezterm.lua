e062f1adb166ed8949b08f98f9e237b5094b0927
XXiaoA <isxxiaoa@gmail.com>
2022-07-28 15:01:13 +0800

    wezterm update

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,16 +1,27 @@
-local wezterm = require("wezterm")
+local wt = require("wezterm")
+
+local scheme = wt.get_builtin_color_schemes()["Andromeda"]
+scheme.cursor_bg = "#8f3f71"
+scheme.cursor_fg = "black"
 
 return {
-    -- You can specify some parameters to influence the font selection;
-    -- for example, this selects a Bold, Italic font variant.
-    -- font = wezterm.font("JetBrains Mono", { weight = "Bold", italic = true }),
-    font = wezterm.font("JetBrains Mono"),
-    font_size = 13.0,
-    color_scheme = "Gruvbox Dark",
+    enable_tab_bar = false,
+    font = wt.font("JetBrainsMono Nerd Font", { weight = 325 }),
+    font_size = 16,
+    color_schemes = {
+        ["Andromeda"] = scheme,
+    },
+    color_scheme = "Andromeda",
+    custom_block_glyphs = true,
+    window_decorations = "RESIZE",
+    window_close_confirmation = "NeverPrompt",
+    cursor_blink_ease_in = "Constant",
+    cursor_blink_ease_out = "Constant",
+
     keys = {
         {
             key = "F11",
-            action = wezterm.action.ToggleFullScreen,
+            action = wt.action.ToggleFullScreen,
         },
     },
 }
