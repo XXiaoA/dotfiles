bbf14b07d7a9c8851fe37bb09f3192fc3b5d5960
XXiaoA <isxxiaoa@gmail.com>
2022-07-29 21:58:31 +0800

    update

--- fish/.config/fish/functions/nvim_nightly.fish
+++ fish/.config/fish/functions/nvim_nightly.fish
@@ -8,6 +8,10 @@ function nvim_nightly --description "Neovim Nightly"
             wget -x https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage -O $DOWNLOAD_PATH
             chmod u+x $DOWNLOAD_PATH
         case "*"
-            $DOWNLOAD_PATH $argv
+            if test -e $DOWNLOAD_PATH
+                $DOWNLOAD_PATH $argv
+            else
+                echo "Please run the command after running 'nvim_nightly --update'"
+            end
     end
 end
