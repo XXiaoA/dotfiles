822bb37c3e4b1caa4f8ab2cf83b6a56adaae71cc
XXiaoA <isxxiaoa@gmail.com>
2022-08-11 17:54:35 +0800

    save

--- fish/.config/fish/functions/rm.fish
+++ fish/.config/fish/functions/rm.fish
@@ -2,6 +2,8 @@ function rm --description Remove
     if command -sq trash-put
         echo "You're using trash-cli" && trash-put $argv && echo "Removed $argv"
     else
-        echo "Please use trash-cli" && rm $argv && echo "Removed $argv"
+        set_color red; echo "Please use trash-cli"
+        set_color normal
+        /usr/bin/rm $argv && echo "Removed $argv"
     end
 end
