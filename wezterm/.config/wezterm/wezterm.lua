edec0bc9174ef5c8592b45470556e8356636be26
XXiaoA <isxxiaoa@gmail.com>
2022-08-14 17:23:36 +0800

    wezterm

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,27 +1,35 @@
-local wt = require("wezterm")
+local wezterm = require("wezterm")
 
-local scheme = wt.get_builtin_color_schemes()["Andromeda"]
+local scheme = wezterm.get_builtin_color_schemes()["Andromeda"]
 scheme.cursor_bg = "#8f3f71"
 scheme.cursor_fg = "black"
 
 return {
-    enable_tab_bar = false,
-    font = wt.font("JetBrainsMono Nerd Font"),
+    font = wezterm.font({
+        family = "JetBrainsMono Nerd Font",
+        harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
+    }),
+
     font_size = 16,
+
     color_schemes = {
         ["Andromeda"] = scheme,
     },
     color_scheme = "Andromeda",
-    custom_block_glyphs = true,
+
+    window_background_opacity = 0.95,
     window_decorations = "RESIZE",
     window_close_confirmation = "NeverPrompt",
+
+    enable_tab_bar = false,
+
     cursor_blink_ease_in = "Constant",
     cursor_blink_ease_out = "Constant",
 
     keys = {
         {
             key = "F11",
-            action = wt.action.ToggleFullScreen,
+            action = wezterm.action.ToggleFullScreen,
         },
     },
 }
