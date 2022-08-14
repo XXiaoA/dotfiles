93ecc6ecdecc2a9f406e97de5f05b0a145857760
XXiaoA <isxxiaoa@gmail.com>
2022-08-14 17:09:55 +0800

    fish save

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -20,15 +20,10 @@ set -g fish_user_paths ~/.local/share/nvim/mason/bin/ $fish_user_paths
 set -g fish_user_paths ~/.local/bin/ $fish_user_paths
 
 fish_vi_key_bindings
+set -g fish_vi_force_cursor 1
 set fish_cursor_insert line
 set fish_cursor_replace_one underscore
 set fish_cursor_replace underscore
-# https://issuehint.com/issue/fish-shell/fish-shell/8981
-if status is-interactive
-    if string match -q -- 'tmux*' $TERM
-        set -g fish_vi_force_cursor 1
-    end
-end
 
 starship init fish | source
 zoxide init fish | source
