9cb67cda3741710abc79b8511c9ad3b094c6ce35
XXiaoA <isxxiaoa@gmail.com>
2023-04-14 19:56:40 +0800

    fish: add alias for `clip feature`

--- fish/conf.d/alias.fish
+++ fish/conf.d/alias.fish
@@ -43,3 +43,5 @@ alias tk "tmux kill-session -t"
 alias tka='tmux kill-server'
 
 alias color_test "curl https://gist.githubusercontent.com/lilydjwg/fdeaf79e921c2f413f44b6f613f6ad53/raw/94d8b2be62657e96488038b0e547e3009ed87d40/colors.py | python"
+
+alias clip='curl -F "c=@-" "https://fars.ee/?u=1"'
