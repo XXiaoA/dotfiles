7646ec6ed257307057b8f208ad6c0c1ccc0e4035
XXiaoA <isxxiaoa@gmail.com>
2023-07-12 15:15:31 +0800

    fish(alias): change alias for `exa`

--- fish/conf.d/alias.fish
+++ fish/conf.d/alias.fish
@@ -18,10 +18,9 @@ end
 if command -sq exa
     alias ls="exa --color=auto"
     alias l='exa -lbh --icons'
-    alias la='exa -labgh --icons'
-    alias ll='exa -lbg --icons'
-    alias lsa='exa -lbagR --icons'
-    alias lst='exa -lTabgh --icons' # 输入lst,将展示类似于tree的树状列表。
+    alias la='exa -labh --icons'
+    alias lsa='exa -lbaR --icons'
+    alias lst='exa -lTabh --icons' # 输入lst,将展示类似于tree的树状列表。
 
     alias tree "exa -T"
 
@@ -44,4 +43,5 @@ alias tka='tmux kill-server'
 
 alias color_test "curl https://gist.githubusercontent.com/lilydjwg/fdeaf79e921c2f413f44b6f613f6ad53/raw/94d8b2be62657e96488038b0e547e3009ed87d40/colors.py | python"
 
+# usage: command | clip
 alias clip='curl -F "c=@-" "https://fars.ee/?u=1"'
