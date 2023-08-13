aef78e442fe77619b9fd4c0889ed8815ae1f7a7f
XXiaoA <isxxiaoa@gmail.com>
2023-08-13 23:20:38 +0800

    keymap remove `jj` use `C-[` instead

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -1,5 +1,4 @@
 function fish_user_key_bindings
-    bind -M insert -m default jj backward-char force-repaint
     bind --preset H beginning-of-line
     bind --preset L end-of-line
     bind --preset -M visual H beginning-of-line
