38ac52e656db1d064cd96eefe9df3979cfa695e9
XXiaoA <isxxiaoa@gmail.com>
2024-04-06 11:25:54 +0800

    tmux: add `tmux-sessionizer`

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -5,4 +5,6 @@ function fish_user_key_bindings
     bind --preset -M visual H beginning-of-line
     bind --preset -M visual L end-of-line
     bind --preset -M visual -m default v end-selection repaint-mode
+
+    bind -M insert \cf tmux-sessionizer
 end
