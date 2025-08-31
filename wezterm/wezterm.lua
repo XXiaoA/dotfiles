6f864b2ccad3ee306080306883dda2040d18f612
XXiaoA <isxxiaoa@gmail.com>
2025-08-31 14:58:33 +0800

    wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,5 +1,7 @@
 ---@diagnostic disable: unused-local
 local wezterm = require("wezterm")
+local act = wezterm.action
+local config = {}
 
 local function modify_colorscheme(colorscheme)
     local file_path = os.getenv("HOME") .. "/.config/wezterm/wezterm.lua"
@@ -27,7 +29,15 @@ end)
 
 local color_scheme = "Everforest"
 
-return {
+wezterm.on("update-right-status", function(window, pane)
+    local leader = ""
+    if window:leader_is_active() then
+        leader = "🤔"
+    end
+    window:set_right_status(leader)
+end)
+
+config = {
     font = wezterm.font_with_fallback({
         {
             family = "Maple Mono",
@@ -51,7 +61,7 @@ return {
         "MesloLGSDZ Nerd Font Mono",
         "feather",
     }),
-    font_size = 16.5,
+    font_size = 13.5,
     allow_square_glyphs_to_overflow_width = "Never",
 
     color_scheme = color_scheme,
@@ -60,7 +70,7 @@ return {
 
     window_padding = {
         left = 10,
-        right = 2,
+        right = 0,
         top = 0,
         bottom = 0,
     },
@@ -102,12 +112,54 @@ return {
     },
 
     disable_default_key_bindings = true,
+    -- leader = { key = "z", mods = "CTRL", timeout_milliseconds = 2000 },
     keys = {
-        { key = "Enter", mods = "SHIFT", action = wezterm.action.SendString("\x1b[13;2u") },
-        { key = "0", mods = "CTRL", action = wezterm.action.ResetFontSize },
-        { key = "=", mods = "CTRL", action = wezterm.action.IncreaseFontSize },
-        { key = "-", mods = "CTRL", action = wezterm.action.DecreaseFontSize },
-        { key = "c", mods = "CTRL|SHIFT", action = wezterm.action.CopyTo("Clipboard") },
-        { key = "v", mods = "CTRL|SHIFT", action = wezterm.action.PasteFrom("Clipboard") },
+        -- stylua: ignore start
+        { mods = 'LEADER', key = '0', action = wezterm.action.PaneSelect { mode = 'SwapWithActive' } },
+        { key = ",", mods = "LEADER",
+            action = act.PromptInputLine({
+                description = "Enter new name for tab",
+                action = wezterm.action_callback(function(window, pane, line)
+                    if line then
+                        window:active_tab():set_title(line)
+                    end
+                end),
+            }),
+        },
+        { key = "\\", mods = "LEADER", action = act.SplitPane({ direction = "Right" }) },
+        { key = "-", mods = "LEADER", action = act.SplitPane({ direction = "Down" }) },
+        { key = "z", mods = "LEADER", action = "TogglePaneZoomState" },
+        { key = "w", mods = "LEADER", action = act.ShowTabNavigator },
+        { key = "l", mods = "LEADER", action = act.ActivateTabRelative(1) },
+        { key = "c", mods = "LEADER", action = act.SpawnTab("CurrentPaneDomain") },
+        { key = "v", mods = "LEADER", action = act.ActivateCopyMode },
+        { key = "r", mods = "LEADER", action = act.ReloadConfiguration },
+        { key = "1", mods = "LEADER", action = act({ ActivateTab = 0 }) },
+        { key = "2", mods = "LEADER", action = act({ ActivateTab = 1 }) },
+        { key = "3", mods = "LEADER", action = act({ ActivateTab = 2 }) },
+        { key = "4", mods = "LEADER", action = act({ ActivateTab = 3 }) },
+        { key = "5", mods = "LEADER", action = act({ ActivateTab = 4 }) },
+        { key = "6", mods = "LEADER", action = act({ ActivateTab = 5 }) },
+        { key = "7", mods = "LEADER", action = act({ ActivateTab = 6 }) },
+        { key = "8", mods = "LEADER", action = act({ ActivateTab = 7 }) },
+        { key = "9", mods = "LEADER", action = act({ ActivateTab = 8 }) },
+        { key = "x", mods = "LEADER", action = act({ CloseCurrentPane = { confirm = true } }) },
+
+        { key = "Enter", mods = "ALT", action = act.ToggleFullScreen },
+        { key = "Enter", mods = "SHIFT", action = act.SendString("\x1b[13;2u") },
+        { key = "0", mods = "CTRL", action = act.ResetFontSize },
+        { key = "=", mods = "CTRL", action = act.IncreaseFontSize },
+        { key = "-", mods = "CTRL", action = act.DecreaseFontSize },
+        { key = "c", mods = "CTRL|SHIFT", action = act.CopyTo("Clipboard") },
+        { key = "v", mods = "CTRL|SHIFT", action = act.PasteFrom("Clipboard") },
+        -- stylua: ignore end
     },
 }
+
+local is_win = wezterm.target_triple:find("window") ~= nil
+if is_win then
+    config.default_domain = "WSL:Ubuntu"
+    config.hide_tab_bar_if_only_one_tab = false
+end
+
+return config
